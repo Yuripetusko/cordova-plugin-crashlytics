210fa0bb6db35175912b8e6c17578a28a95bcfea
Yuri Petusko <yuripetusko@gmail.com>
2018-02-04 22:24:39 +0000

    export plugin under name crashlyticsPlugin

--- www/android/CrashlyticsPlugin.js
+++ www/android/CrashlyticsPlugin.js
@@ -1,4 +1,3 @@
-
 /**
  * Constructor
  */
@@ -7,144 +6,104 @@ function CrashlyticsPlugin() {
 }
 
 CrashlyticsPlugin.prototype.addLog = function(message) {
-  
-	var defaults = {
-        message: message
-    };
+  var defaults = {
+    message: message,
+  };
 
-	cordova.exec(null, 
-		null, 
-		"CrashlyticsPlugin", 
-		"addLog",
-		[defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "addLog", [defaults]);
 };
 
 CrashlyticsPlugin.prototype.sendCrash = function(message) {
-  
-	var defaults = {message: message};
+  var defaults = { message: message };
 
-	cordova.exec(null, 
-		null, 
-		"CrashlyticsPlugin", 
-		"sendCrash",
-		[defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "sendCrash", [defaults]);
 };
 
 CrashlyticsPlugin.prototype.sendNonFatalCrash = function(message) {
-  
-	var defaults = {message: message};
+  var defaults = { message: message };
 
-	cordova.exec(null, 
-		null, 
-		"CrashlyticsPlugin", 
-		"sendNonFatalCrash",
-		[defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "sendNonFatalCrash", [
+    defaults,
+  ]);
 };
 
 CrashlyticsPlugin.prototype.setUserIdentifier = function(userIdentifier) {
+  var defaults = {
+    value: userIdentifier,
+  };
 
-    var defaults = {
-      value: userIdentifier
-    };
-
-    cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setUserIdentifier",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setUserIdentifier", [
+    defaults,
+  ]);
 };
 
 CrashlyticsPlugin.prototype.setUserName = function(userName) {
+  var defaults = {
+    value: userName,
+  };
 
-    var defaults = {
-      value: userName
-    };
-
-    cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setUserName",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setUserName", [defaults]);
 };
 
 CrashlyticsPlugin.prototype.setUserEmail = function(userEmail) {
+  var defaults = {
+    value: userEmail,
+  };
 
-    var defaults = {
-      value: userEmail
-    };
-
-    cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setUserEmail",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setUserEmail", [defaults]);
 };
 
 CrashlyticsPlugin.prototype.setStringValueForKey = function(value, key) {
+  var defaults = {
+    value: value,
+    key: key,
+  };
 
-    var defaults = {
-      value: value,
-      key: key
-    };
-
-     cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setStringValueForKey",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setStringValueForKey", [
+    defaults,
+  ]);
 };
 
 CrashlyticsPlugin.prototype.setIntValueForKey = function(value, key) {
+  var defaults = {
+    value: value,
+    key: key,
+  };
 
-    var defaults = {
-      value: value,
-      key: key
-    };
-
-     cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setIntValueForKey",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setIntValueForKey", [
+    defaults,
+  ]);
 };
 
 CrashlyticsPlugin.prototype.setBoolValueForKey = function(value, key) {
+  var defaults = {
+    value: value,
+    key: key,
+  };
 
-    var defaults = {
-      value: value,
-      key: key
-    };
-
-     cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setBoolValueForKey",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setBoolValueForKey", [
+    defaults,
+  ]);
 };
 
 CrashlyticsPlugin.prototype.setFloatValueForKey = function(value, key) {
+  var defaults = {
+    value: value,
+    key: key,
+  };
 
-    var defaults = {
-      value: value,
-      key: key
-    };
-
-     cordova.exec(null, 
-      null, 
-      "CrashlyticsPlugin", 
-      "setFloatValueForKey",
-      [defaults]);
+  cordova.exec(null, null, "CrashlyticsPlugin", "setFloatValueForKey", [
+    defaults,
+  ]);
 };
-
-
 
 var crashlyticsPanel = new CrashlyticsPlugin();
 module.exports = crashlyticsPanel;
 
 // Make plugin work under window.plugins
 if (!window.plugins) {
-    window.plugins = {};
+  window.plugins = {};
 }
-if (!window.plugins.crashlyticsPanel) {
-    window.plugins.crashlyticsPanel = crashlyticsPanel;
+if (!window.plugins.crashlyticsPlugin) {
+  window.plugins.crashlyticsPlugin = crashlyticsPanel;
 }
